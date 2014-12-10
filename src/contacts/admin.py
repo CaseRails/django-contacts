cf44079796f2777252fadd5dfb81d4ecc92b8cd1
Erik Dykema <erik@caserails.com>
2014-12-10 13:55:40 -0500

    Fixes for Django 1.7

--- src/contacts/admin.py
+++ src/contacts/admin.py
@@ -1,86 +1,90 @@
 from django.contrib import admin
-from django.contrib.contenttypes import generic
-from django.contrib.comments.models import Comment
+# from django.contrib.contenttypes import generic
 
-from contacts.models import Company, Person, Group, PhoneNumber, EmailAddress, InstantMessenger, WebSite, StreetAddress, SpecialDate, Location
+# from django.contrib.comments.models import Comment
 
-class EmailAddressInline(generic.GenericTabularInline):
-	model = EmailAddress
+from contacts.models import (Company, Person, Group,
+                             PhoneNumber, EmailAddress,
+                             InstantMessenger, WebSite,
+                             StreetAddress, SpecialDate,
+                             Location)
 
-class PhoneNumberInline(generic.GenericTabularInline):
-	model = PhoneNumber
+class EmailAddressInline(admin.TabularInline):
+        model = EmailAddress
 
-class InstantMessengerInline(generic.GenericTabularInline):
-	model = InstantMessenger
+class PhoneNumberInline(admin.TabularInline):
+        model = PhoneNumber
 
-class WebSiteInline(generic.GenericTabularInline):
-	model = WebSite
+class InstantMessengerInline(admin.TabularInline):
+        model = InstantMessenger
 
-class StreetAddressInline(generic.GenericStackedInline):
-	model = StreetAddress
+class WebSiteInline(admin.TabularInline):
+        model = WebSite
 
-class SpecialDateInline(generic.GenericStackedInline):
-	model = SpecialDate
+class StreetAddressInline(admin.StackedInline):
+        model = StreetAddress
 
-class CommentInline(generic.GenericStackedInline):
-	model = Comment
-	ct_fk_field = 'object_pk'
+class SpecialDateInline(admin.StackedInline):
+        model = SpecialDate
+
+# class CommentInline(admin.StackedInline):
+#         model = Comment
 
 class CompanyAdmin(admin.ModelAdmin):
-	inlines = [
-		PhoneNumberInline,
-		EmailAddressInline,
-		InstantMessengerInline,
-		WebSiteInline,
-		StreetAddressInline,
-		SpecialDateInline,
-		CommentInline,
-	]
-	
-	list_display = ('name',)
-	search_fields = ['^name',]
-	prepopulated_fields = {'slug': ('name',)}
+        inlines = [
+                PhoneNumberInline,
+                EmailAddressInline,
+                InstantMessengerInline,
+                WebSiteInline,
+                StreetAddressInline,
+                SpecialDateInline
+                # CommentInline,
+        ]
+
+        list_display = ('name',)
+        search_fields = ['^name',]
+        prepopulated_fields = {'slug': ('name',)}
 
 class PersonAdmin(admin.ModelAdmin):
-	inlines = [
-		PhoneNumberInline,
-		EmailAddressInline,
-		InstantMessengerInline,
-		WebSiteInline,
-		StreetAddressInline,
-		SpecialDateInline,
-		CommentInline,
-	]
-	
-	list_display_links = ('first_name', 'last_name',)
-	list_display = ('first_name', 'last_name', 'company',)
-	list_filter = ('company',)
-	ordering = ('last_name', 'first_name')
-	search_fields = ['^first_name', '^last_name', '^company__name']
-	prepopulated_fields = {'slug': ('first_name', 'last_name')}
+        inlines = [
+                PhoneNumberInline,
+                EmailAddressInline,
+                InstantMessengerInline,
+                WebSiteInline,
+                StreetAddressInline,
+                SpecialDateInline
+                # CommentInline,
+        ]
+
+        list_display_links = ('first_name', 'last_name',)
+        list_display = ('first_name', 'last_name', 'company',)
+        list_filter = ('company',)
+        ordering = ('last_name', 'first_name')
+        search_fields = ['^first_name', '^last_name', '^company__name']
+        prepopulated_fields = {'slug': ('first_name', 'last_name')}
 
 class GroupAdmin(admin.ModelAdmin):
-	list_display_links = ('name',)
-	list_display = ('name', 'date_modified')
-	ordering = ('-date_modified', 'name',)
-	search_fields = ['^name', '^about',]
-	prepopulated_fields = {'slug': ('name',)}
+        list_display_links = ('name',)
+        list_display = ('name', 'date_modified')
+        ordering = ('-date_modified', 'name',)
+        search_fields = ['^name', '^about',]
+        prepopulated_fields = {'slug': ('name',)}
 
 class LocationAdmin(admin.ModelAdmin):
-	list_display_links = ('name',)
-	list_display = ('name', 'date_modified')
-	ordering = ('weight', 'name')
-	search_fields = ['^name',]
-	prepopulated_fields = {'slug': ('name',)}
-	
-	fieldsets = (
-		(None, {
-			'fields': (('name', 'slug',),)
-		}),
-		('Advanced options', {
-			'fields': (('is_phone', 'is_street_address'),)
-		})
-	)
+        list_display_links = ('name',)
+        list_display = ('name', 'date_modified')
+        ordering = ('weight', 'name')
+        search_fields = ['^name',]
+        prepopulated_fields = {'slug': ('name',)}
+
+        fieldsets = (
+                (None, {
+                        'fields': (('name', 'slug',),)
+                }),
+                ('Advanced options', {
+                        'fields': (('is_phone', 'is_street_address'),)
+                })
+        )
 
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Person, PersonAdmin)
